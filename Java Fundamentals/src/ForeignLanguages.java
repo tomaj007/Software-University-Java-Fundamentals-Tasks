e67d6f19d22e6039bc1e4c95042d3fcea36ae361
Tonkisa24 <72923374+Tonkisa24@users.noreply.github.com>
2023-09-24 21:47:11 +0300

    5th task

---
+++ Java Fundamentals/src/ForeignLanguages.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ForeignLanguages {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        String country =input.nextLine();
+        if(country.equals("USA")||country.equals("England")){
+            System.out.println("English");
+        }else if(country.equals("Spain")||country.equals("Argentina")||country.equals("Mexico")){
+            System.out.println("Spanish");
+        }else{
+            System.out.println("unknown");
+        }
+
+    }
+}
