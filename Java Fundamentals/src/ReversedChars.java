5b1e018bce9761be5ac74c645fac44c9c1437797
Tonkisa24 <72923374+Tonkisa24@users.noreply.github.com>
2023-09-30 22:08:11 +0300

    task

---
+++ Java Fundamentals/src/ReversedChars.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ReversedChars {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        char cr1 =scanner.next().charAt(0);
+        char cr2 =scanner.next().charAt(0);
+        char cr3 =scanner.next().charAt(0);
+        System.out.println(cr3+" "+cr2+" "+cr1);
+    }
+}
